dc38c9d9fce87c03e12aec27e3bc8e92b6c9cc4a
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-14 18:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 function calculateMinCost() {
-  var str = document.getElementById('rope-lengths').value;
-  var arr = str.split(',');
-  var ropes = arr.map(function(str) {
-    return parseInt(str);
-  });
-
-  // Helper function to get the minimum value and its index from an array
-  function getMin(arr) {
-    var min = arr[0];
-    var minIndex = 0;
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] < min) {
-        min = arr[i];
-        minIndex = i;
-      }
-    }
-    return { value: min, index: minIndex };
-  }
-
-  var totalCost = 0;
-  while (ropes.length > 1) {
-    var min1 = getMin(ropes);
-    ropes.splice(min1.index, 1);
-    var min2 = getMin(ropes);
-    ropes.splice(min2.index, 1);
-    var sum = min1.value + min2.value;
-    totalCost += sum;
-    ropes.push(sum);
-  }
-
-  document.getElementById('result').innerHTML = totalCost;
+  //your code here
+  let string = document.getElementById("rope-lengths").value
+	
+	let arr = string.split(",");
+	let finalValue =0;
+	while(arr.length > 1){
+		arr.sort((a,b) =>{return(a-b)});
+		let value = parseInt (arr.shift());
+		let value1 = parseInt(arr.shift());
+		let mainValue = value+value1;
+		finalValue = finalValue + mainValue;
+		arr.push(mainValue);
+		
+	}
+  let result = document.getElementById("result");
+	result.innerText = finalValue;
+	return finalValue;
+			
 }
