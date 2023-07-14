19a917deaa37e6dc8342f256c5e191a024f2127d
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-14 18:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  var str = document.getElementById('rope-lengths').value;
+  var arr = str.split(',');
+  var ropes = arr.map(function(str) {
+    return parseInt(str);
+  });
+
+  // Helper function to get the minimum value and its index from an array
+  function getMin(arr) {
+    var min = arr[0];
+    var minIndex = 0;
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+      }
+    }
+    return { value: min, index: minIndex };
+  }
+
+  var totalCost = 0;
+  while (ropes.length > 1) {
+    var min1 = getMin(ropes);
+    ropes.splice(min1.index, 1);
+    var min2 = getMin(ropes);
+    ropes.splice(min2.index, 1);
+    var sum = min1.value + min2.value;
+    totalCost += sum;
+    ropes.push(sum);
+  }
+
+  document.getElementById('result').innerHTML = totalCost;
+}
