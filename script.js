93cb2ba44390839b0cf96adcc28cc3b98faaae97
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-18 19:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,94 +1,42 @@
-function minCostToConnectRopes(arr) {
-  // Create a min-heap
-  const heap = new MinHeap();
-  
-  // Insert all the rope lengths into the heap
-  for (let i = 0; i < arr.length; i++) {
-    heap.insert(arr[i]);
-  }
-  
-  let totalCost = 0;
-  
-  while (heap.size() > 1) {
-    // Extract the two smallest elements from the heap
-    const min1 = heap.extractMin();
-    const min2 = heap.extractMin();
-    
-    const cost = min1 + min2;
-    totalCost += cost;
-    
-    // Insert the sum back into the heap
-    heap.insert(cost);
-  }
-  
-  return totalCost;
-}
-
-// Implementation of a MinHeap data structure
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-  
-  size() {
-    return this.heap.length;
-  }
-  
-  insert(val) {
-    this.heap.push(val);
-    this.bubbleUp(this.heap.length - 1);
-  }
-  
-  extractMin() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-    
-    const min = this.heap[0];
-    const lastElement = this.heap.pop();
-    
-    if (this.heap.length > 0) {
-      this.heap[0] = lastElement;
-      this.sinkDown(0);
-    }
-    
-    return min;
-  }
-  
-  bubbleUp(index) {
-    const parentIdx = Math.floor((index - 1) / 2);
-    
-    if (parentIdx >= 0 && this.heap[index] < this.heap[parentIdx]) {
-      [this.heap[index], this.heap[parentIdx]] = [this.heap[parentIdx], this.heap[index]];
-      this.bubbleUp(parentIdx);
-    }
-  }
-  
-  sinkDown(index) {
-    const leftChildIdx = 2 * index + 1;
-    const rightChildIdx = 2 * index + 2;
-    let smallest = index;
-    
-    if (leftChildIdx < this.heap.length && this.heap[leftChildIdx] < this.heap[smallest]) {
-      smallest = leftChildIdx;
-    }
-    
-    if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[smallest]) {
-      smallest = rightChildIdx;
-    }
-    
-    if (smallest !== index) {
-      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-      this.sinkDown(smallest);
-    }
+function compare(a, b) {
+  if (a < b) {
+      return -1;
+  } else if (a > b) {
+      return 1;
+  } else {
+      return 0;
   }
 }
-
-// Example usage
-const n = promp();
-const N = Number.parseInt(n);
-for(let i=0;i<N;i++){
-	ropes[i]= prompt();
+ 
+ 
+ 
+function calculateMinCost() {
+  //your code here
+  let str=document.getElementById('rope-lengths').value.split(",");
+  let arr=str.map((str)=>parseInt(str));
+  // console.log(arr);
+  
+let total=0;
+arr=arr.sort(compare);
+// console.log("arr ",arr);
+while(arr.length>=2){
+  let sum=arr[0]+arr[1];
+  // console.log(sum);
+  let rem=[];
+  rem.push(sum);
+  for(let k=2;k<arr.length;k++){
+    rem.push(arr[k]);
+  }
+  rem=rem.sort(compare);
+  // console.log("rem ",rem);
+  arr=[...rem];
+  // console.log("arr ",arr);
+  total=total+sum;
+  // console.log("total "+total+"----------------------");
 }
-const minCost = minCostToConnectRopes(ropes);
-console.log(minCost);
+console.log(total);
+ 
+ 
+let result=document.getElementById('result');
+result.innerHTML=total;
+}  
