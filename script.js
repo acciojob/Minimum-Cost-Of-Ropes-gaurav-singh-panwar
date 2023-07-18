c1cb862f052ed464c99066462cf7454c6a7e2c9f
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-18 19:29:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,96 @@
-function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
- 
- 
- 
 function calculateMinCost() {
-  //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
+  let str = document.getElementById('rope-lengths').value;
+  let arr = str.split(",").map(Number);
   
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
+  let total = 0;
+  let heap = new MinHeap(arr); // Create a min heap
+  
+  while (heap.size() > 1) {
+    let first = heap.extractMin(); // Extract the smallest element
+    let second = heap.extractMin(); // Extract the next smallest element
+    
+    let sum = first + second;
+    total += sum;
+    
+    heap.insert(sum); // Insert the sum back into the heap
   }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
+  
+  let result = document.getElementById('result');
+  result.innerHTML = total;
 }
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
-}  
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+  
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+  
+  extractMin() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    
+    const min = this.heap[0];
+    const last = this.heap.pop();
+    
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.heapifyDown();
+    }
+    
+    return min;
+  }
+  
+  heapifyUp() {
+    let currentIndex = this.heap.length - 1;
+    
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+      
+      if (this.heap[parentIndex] <= this.heap[currentIndex]) {
+        break;
+      }
+      
+      [this.heap[parentIndex], this.heap[currentIndex]] = [this.heap[currentIndex], this.heap[parentIndex]];
+      currentIndex = parentIndex;
+    }
+  }
+  
+  heapifyDown() {
+    let currentIndex = 0;
+    let childIndex;
+    
+    while (true) {
+      const leftChildIndex = 2 * currentIndex + 1;
+      const rightChildIndex = 2 * currentIndex + 2;
+      let swapIndex = null;
+      
+      if (leftChildIndex < this.heap.length) {
+        if (this.heap[leftChildIndex] < this.heap[currentIndex]) {
+          swapIndex = leftChildIndex;
+        }
+      }
+      
+      if (rightChildIndex < this.heap.length) {
+        if (this.heap[rightChildIndex] < this.heap[currentIndex] && this.heap[rightChildIndex] < this.heap[leftChildIndex]) {
+          swapIndex = rightChildIndex;
+        }
+      }
+      
+      if (swapIndex === null) {
+        break;
+      }
+      
+      [this.heap[currentIndex], this.heap[swapIndex]] = [this.heap[swapIndex], this.heap[currentIndex]];
+      currentIndex = swapIndex;
+    }
+  }
+  
+  size() {
+    return this.heap.length;
+  }
+}
